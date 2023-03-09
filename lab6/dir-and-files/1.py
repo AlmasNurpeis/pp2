56ea7e54403caef1cb6b1d257245f2fb95dfc3a0
Almas <a_nurpeis@kbtu.kz>
2023-03-09 17:44:55 +0600

    a

--- lab6/dir-and-files/1.py
+++ lab6/dir-and-files/1.py
@@ -1,11 +1,8 @@
 import os
-path = input("Enter path: ")
-directories = [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+path = "C:\\Users\\Lenovo\\Desktop\\MyGitProj\\"
 print("Directories in path:")
-print(directories)
-files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+print([d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))])
 print("Files in path:")
-print(files)
-all = os.listdir(path)
+print([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
 print("All contents in path:")
-print(all)
+print(os.listdir(path))
